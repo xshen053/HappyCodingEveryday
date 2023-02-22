b579f5dd33b06c9787374cc9ce64681aa679a843
xshen053 <shenxiaxi26@gmail.com>
2023-02-21 16:09:50 -0800

    finished 5.04 python

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -4,9 +4,18 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
-
+    # {precondition: i = 0 is reachable}
+    assert(len(A) != 0)
+    # {so max_distance = 0 is fine}
+    max_distance = 0
+    # {inv: max_distance = max from[0, i - 1]}
+    for i in range(len(A)):
+        if (i <= max_distance):
+            # {max from [0,  i - 1] && i <= max_distance}
+            max_distance = max(i + A[i], max_distance)
+            # {max_distance = max from[0, i]}
+        # {inv: max_distance = max from[0, i - 1]}
+    return max_distance >= len(A) - 1
 
 if __name__ == '__main__':
     exit(

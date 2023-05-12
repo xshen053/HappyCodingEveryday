e3d48814263239c4f1adabec9433b921077192cd
xshen053 <shenxiaxi26@gmail.com>
2023-05-12 00:35:46 -0700

    finished 8.2, 8.3

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -2,8 +2,54 @@ from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    # STEP 0 split string
+    stack = expression.split(",")
+
+    # STEP 1 Edge cases
+    if len(stack) == 1:
+        return int(stack[0])
+    
+    # {stack: [3, 4, +, 2, *, 1, +]}
+
+    # STEP 2 cacluate using a stack, make sure there are
+    stack.reverse()
+    # {[+, 1, *, 2, +, 4, 3]}
+    result = 0
+    # print(stack)
+
+    # {inv: [0, len(stack)) haven't processed}
+
+    # 
+    cal_stack = []
+    while len(stack) != 0:
+        # wrong place to define cal_stack
+        # cal_stack = []
+        while 1:
+            if (stack[-1] == "+" or stack[-1] == "-" or stack[-1] == "*" or stack[-1] == "/"):
+                break
+            cal_stack.append(stack.pop())
+            
+        # {cal_stack has numbers && stack[-1] is operator}
+        op = stack.pop()
+        j = cal_stack.pop()
+        i = cal_stack.pop()
+        i, j = int(i), int(j)
+        temp = 0
+        if (op == "+"):
+            temp = i + j
+        elif (op == "-"):
+            temp = i - j
+        elif (op == "*"):
+            temp = i * j
+        elif (op == "/"):
+            temp = i // j
+        cal_stack.append(temp)
+
+    return cal_stack.pop()
+
+    
+
+    
 
 
 if __name__ == '__main__':
--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    if len(s) == 0:
+        return True    
+    stack = []
+    lookup = {'(':')', '{':'}', '[':']'}
+
+    for c in s:
+        if c in lookup:
+            stack.append(c)
+        elif not stack or lookup[stack.pop()] != c:
+            # not stack 因为elif的时候，c必定是), }, ]其中的一个，要匹配成功，必须stack不为空，否则肯定有问题
+            # 如果先输入} ] )肯定就错了
+            # 输入的}，stack里是(或[，那肯定也错了
+            return False
+
+    return not stack
 
 
 if __name__ == '__main__':

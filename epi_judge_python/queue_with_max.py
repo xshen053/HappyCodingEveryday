f20f51b958385b80af8dd3b83ade775956a598e8
xshen053 <shenxiaxi26@gmail.com>
2023-05-21 12:24:48 -0700

    finished 8.9

--- epi_judge_python/queue_with_max.py
+++ epi_judge_python/queue_with_max.py
@@ -1,19 +1,35 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
-
+import collections
 
 class QueueWithMax:
+    def __init__(self) -> None:
+        self._entries = collections.deque()
+        self._candidates_for_max = collections.deque()
+
     def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self._entries.append(x)
+        # Eliminate dominated elements in _candidates_for_max.
+        while self._candidates_for_max and self._candidates_for_max[-1] < x:
+            self._candidates_for_max.pop()
+        self._candidates_for_max.append(x)
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        # None case
+        if not self._entries:
+            raise IndexError('empty queue')
+        
+        # Normal case
+        num = self._entries.popleft()
+        if num == self._candidates_for_max[0]:
+            self._candidates_for_max.popleft()
+        return num
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if not self._candidates_for_max:
+            raise IndexError('empty queue')
+        
+        return self._candidates_for_max[0]
 
 
 def queue_tester(ops):

0d0f6334460cdad37e71986c14903616fc6aa53c
xshen053 <shenxiaxi26@gmail.com>
2023-05-24 10:36:43 -0700

    finished 9.5, guess preorder?

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -2,9 +2,16 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
-    # TODO - you fill in here.
-    return 0
+def sum_root_to_leaf(tree: BinaryTreeNode, partial_path_sum = 0) -> int:
+    if not tree:
+        return 0
+    partial_path_sum = 2 * partial_path_sum + tree.data
+
+    if not tree.left and not tree.right:
+        return partial_path_sum
+    
+    return (sum_root_to_leaf(tree.left, partial_path_sum) 
+            + sum_root_to_leaf(tree.right, partial_path_sum))
 
 
 if __name__ == '__main__':

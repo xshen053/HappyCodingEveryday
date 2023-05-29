a8f31072cb695c13d2e3091aefc84cbdea03cc9d
xshen053 <shenxiaxi26@gmail.com>
2023-05-28 23:19:56 -0700

    8.3

--- epi_judge_python/is_valid_parenthesization_5_28.py
+++ epi_judge_python/is_valid_parenthesization_5_28.py
@@ -2,19 +2,24 @@ from test_framework import generic_test
 
 
 def is_well_formed(s: str) -> bool:
-    if (len(s) == 0):
-        return True
-    
+    if len(s) == 0:
+        return True    
     stack = []
     lookup = {'(':')', '{':'}', '[':']'}
-    
+
     for c in s:
         if c in lookup:
             stack.append(c)
         elif not stack or lookup[stack.pop()] != c:
+            # Case1:
+            # not stack 因为elif的时候，c必定是), }, ]其中的一个，要匹配成功，必须stack不为空，否则肯定有问题
+            # 如果先输入} ] )肯定就错了
+            # Case2:
+            # 输入的}，stack里是(或[，那肯定也错了
             return False
 
     return not stack
+
 
 if __name__ == '__main__':
     exit(
